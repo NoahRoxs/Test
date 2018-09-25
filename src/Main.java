3a9563c612047924fee076da8ea2ec7ca4ab75b4
NoahRoxs <43574943+NoahRoxs@users.noreply.github.com>
2018-09-25 16:23:55 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.security.KeyStore;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Github!");
 
-        String = "test";
-                
+
+
+        //datetypes: String, int, double, float, boolean, byte//
+
+        String henk = "henk";
+        henk = "piet";
+
+        System.out.println(henk);
+        System.out.println("henk");
+
+        int leeftijd = 39;
+        leeftijd = leeftijd +6;
+        leeftijd +=6;
+        //leeftijd++;
+        System.out.println(leeftijd);
+
+        boolean snapHet = true;
+        System.out.println("ik snap het");
+
+        float snelstetijd = 45.55f;
+        double nogprecizer = 65.66666666664d;
     }
 }
